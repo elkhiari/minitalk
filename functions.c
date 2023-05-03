27f096a7e8db424a6b1f585e6cbcf7275af2270b
Othmane Elkhiari <oelkhiar@e1r3p11.1337.ma>
2023-05-03 15:49:21 +0100

    Add atoi

--- functions.c
+++ functions.c
@@ -6,7 +6,7 @@
 /*   By: oelkhiar <oelkhiar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 14:55:56 by oelkhiar          #+#    #+#             */
-/*   Updated: 2023/05/03 14:56:05 by oelkhiar         ###   ########.fr       */
+/*   Updated: 2023/05/03 15:48:56 by oelkhiar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,4 +32,29 @@ void	ft_putnbr(int n)
 		nbr %= 10;
 	}
 	ft_putchar(nbr % 10 + '0');
+}
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	j;
+	int	nbr;
+
+	i = 1;
+	j = 0;
+	nbr = 0;
+	while (str[j] == '32' || (str[j] >= '9' && str[j] <= '13'))
+		j++;
+	if (str[j] == '-' || str[j] == '+')
+	{
+		if (str[j] == '-')
+			i = -1;
+		j++;
+	}
+	while (str[j] >= '0' && str[j] <= '9')
+	{
+		nbr = nbr * 10 + (str[j] - '0');
+		j++;
+	}
+	return (nbr * i);
 }
